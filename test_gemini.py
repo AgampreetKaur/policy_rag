5539a0aedfbffcd48b1fa3c0ede0b5bd7a59471f
Agam Singh <146024962+agamsinggh@users.noreply.github.com>
2026-06-13 19:17:29 +0530

    Update test_gemini.py

--- test_gemini.py
+++ test_gemini.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AQ.Ab8RN6JHlN4qH5VfqUpxeC6mESzqwv0oD2rcQlW2NhY38P0BPA")
+genai.configure(api_key="Enter your API key")
 
 model = genai.GenerativeModel("gemini-2.0-flash")
 response = model.generate_content("Hello")
